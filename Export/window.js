cbc5015ba271ef55884004bf96cccbdd184fca18
John Eckert <john2023219@live.ytech.edu>
2022-02-10 09:42:57 -0500

    Removing testing
    Swapped the code in testing and window, then deleted testing.

--- Export/window.js
+++ Export/window.js
@@ -1,58 +1,17 @@
-const fs = require("fs");
+const csv = require('csv-to-json');
 const fastcsv = require("fast-csv");
+const fs = require("fs");
+let csvToJson = require('convert-csv-to-json');
 
-let stream = fs.createReadStream("Changes.csv")
-let csvData = [];
-let csvStream = fastcsv
+function conversion(inputfile, outputfile) {
+  let input = inputfile;
+  let output = outputfile;
+  csvToJson.generateJsonFileFromCsv(input,output);
+  console.log('wrote to file')
+}
 
-// require csvtojson
-var csv = require("csvtojson");
-
-// Convert a csv file with csvtojson
-csv()
-  .fromFile('Users/jeckert/OneDrive - York County School of Technology/Documents/GitHub/docpacs2122/Export/data')
-  .then(function(jsonArrayObj){ //when parse finished, result will be emitted here.
-     console.log(jsonArrayObj);
-   })
-
-// Parse large csv with stream / pipe (low mem consumption)
-csv()
-  .fromStream(readableStream)
-  .subscribe(function(jsonObj){ //single json object will be emitted for each csv line
-     // parse each json asynchronousely
-     return new Promise(function(resolve,reject){
-         asyncStoreToDb(json,function(){resolve()})
-     })
-  })
-
-//Use async / await
-const jsonArray = csv().fromFile(filePath);
-
-  .parse()
-  .on("data", function(data) {
-    csvData.push(data);
-  })
-  .on("end", function() {
-    // Remove the first line: header
-    csvData.shift();
-    // Open the connection
-    // connect to the mySQL database
-    // save csvData
-    const mysql = require("mysql");
-    const connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "abc",
-      database: "testdb"
-    });
-    // Open the connection
-    connection.connect(error => {
-      if (error) {
-        console.error(error);
-      } else {
-        let query =
-          "INSERT INTO category ()"
-      }
-    })
-  });
-stream.pipe(csvStream);
+conversion('Events.csv', 'Events.json')
+conversion('Goals.csv', 'Goals.json')
+conversion('ID.csv', 'ID.json')
+conversion('RD.csv', 'RD.json')
+conversion('Changes.csv', 'Changes.json')
