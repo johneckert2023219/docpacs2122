7b2594cee090c5aa6e7d60a589285c3b7a83fb15
JaysinH13 <90346481+JaysinH13@users.noreply.github.com>
2022-02-14 10:21:51 -0500

    app.js updates

--- Export/app.js
+++ Export/app.js
@@ -9,10 +9,10 @@ const PORT = 5000;
 const ejs = require('ejs');
 const express = require('express');
 const app = express();
-const msql = require("mysql")
+const sqlite3 = require('sqlite3');
 
 
-
+let db = new sqlite3.Database('database.db')
 let files = ['Changes.csv', 'Events.csv', 'Goals.csv', 'ID.csv', 'RD.csv']
 let allData = {}
 
@@ -29,11 +29,18 @@ files.forEach((file, i) => {
   })
 });
 
+let db = new sqlite3.Database('database.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
 console.log(allData);
 app.set('view engine' , 'ejs');
 app.get('/', (req,res) =>{
   res.render('home', {
-    bodytext: "Yurrr"
+    bodytext: "Joe"
   })
 })
 app.listen(PORT, () => {
