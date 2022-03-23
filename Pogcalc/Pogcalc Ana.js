ede55e24c91860f5c58f3c06425233686a3d442a
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-03-23 11:05:37 -0400

    Deleting random one

--- Pogcalc/Pogcalc Ana.js
+++ Pogcalc/Pogcalc Ana.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 pogChampData = fs.readFileSync('data.json');
 let data = JSON.parse(pogChampData);
-// Setting empty arrays to store the data from data.json seperately
+
 let pre1990Champs = 0;
 let champPogCount = 0;
 let champYSW = 0;
@@ -10,7 +10,7 @@ let champYSW = 0;
 let epochTime2022 = Date.parse(new Date());
 
 for (const competitor of data) {
-  champYSW += epochTime2022 - Date.parse(competitor.lastWin)
+  champYSW += (epochTime2022 - Date.parse(competitor.lastWin)) / 1000 / 60 /60 / 24 / 365;
   console.log(champYSW)
   champPogCount += competitor.pogsOwned
   if (true) {
