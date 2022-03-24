6331a84534bb48f5427b1b5a06f04c4a8969cc94
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-03-24 14:55:57 -0400

    Hope I finish tomorrow

--- Pogcalc/Digipog Exam.js
+++ Pogcalc/Digipog Exam.js
@@ -4,3 +4,42 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+studentData = readFileSync('dorta.json')
+
+class Student {
+  constructor(name, sequence) {
+    this.name = name;
+    this.id = sequence;
+    this.balance = 0;
+  }
+}
+
+app.get('/view', (req,res) => {
+  let studentID = req.query.id;
+  if (studentID !== 'undefined') {
+    res.render(studentIDList);
+  } else {
+    studentID.find(studentreq.query.id);
+
+  };
+});
+
+app.post('/edit', (req, res) => {
+
+})
+
+app.get('/newStudent', (req, res) => {
+  res.render(newStudent);
+})
+
+app.post('/new', (req, res) => {
+
+})
+
+
+
+
+app.listen(port, () => {
+  console.log('listening')
+});
